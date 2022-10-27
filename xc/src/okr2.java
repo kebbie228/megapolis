bbb2f45d304585c1626191a3c726d621ab4edde7
гк <anton.konovalchenko@gmail.com>
2022-10-27 19:23:44 +0300

    message

---
+++ xc/src/okr2.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class okr2 {
+    /*Контрольная работа:
+            1. Вывести паттерн в виде треугольника заданной высоты:
+            *
+            *  *
+            *    *
+            ********
+            2. Номиналы белорусских монет: 1,2,5,10,20,50.
+    Дана сумма сдачи: 51..99 копеек.
+    Выдать сдачу минимальным количеством монет.
+     */
+
+    public static void main(String[] args) {
+
+        int n= 4;
+        char x='*';
+        for(int i=0;i<=n;i++) {
+            for (int j = 0; j <=i; j++){
+                if(i==n && i!=1)
+                    System.out.print(x);
+                else {
+                    if (j > 0 && j < i) System.out.print(" ");
+                    else System.out.printf("%c", x);
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
